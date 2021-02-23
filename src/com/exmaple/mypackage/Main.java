f34c3273d4c25d02f84d1e4e7fc56e08d5f8193b
Tomek <tomo105@wp.pl>
2021-02-23 19:23:59 +0100

    second star

--- src/com/exmaple/mypackage/Main.java
+++ src/com/exmaple/mypackage/Main.java
@@ -21,11 +21,14 @@ public class Main {
         }
 
         System.out.println(arrayList);
-        System.out.println(findIntegerToSum2020(arrayList));
-
+        System.out.println(findTwoIntegerToSum2020(arrayList));
+        System.out.println(findThreeIntegerToSum2020(arrayList));
     }
 
-    public static int findIntegerToSum2020(ArrayList<Integer> array) {
+    public static int findTwoIntegerToSum2020(ArrayList<Integer> array) {
+        /*
+        In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+         */
         for (int i : array) {
             for (int j : array) {
                 if (i + j == 2020) {
@@ -34,8 +37,26 @@ public class Main {
                 }
             }
         }
-
         return -1;
     }
+
+    public static int findThreeIntegerToSum2020(ArrayList<Integer> array) {
+        /*
+        Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+        In your expense report, what is the product of the three entries that sum to 2020?
+   */
+        for (int i : array) {
+            for (int j : array) {
+                for (int k : array) {
+                    if (i + j + k == 2020) {
+                        System.out.println("Two numbers to sum up to 2020 are: " + i + ", " + j + " and " + k);
+                        return (i * j * k);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
 
 }
